8fec840968f24066fd26e7e91e96c472b13b91dd
JDJG Inc. Official <ghostzap123@gmail.com>
2021-01-02 01:55:05 -0500

    Update http.py

--- asuna_api/http.py
+++ asuna_api/http.py
@@ -13,23 +13,24 @@ class HTTPClient:
     # Send this request to the SR_API_BASE + path
     # path is what comes after the / in the base url
     # **kwargs is passed to self.session.request along with the full url
+
   async def get(self, url, **kwargs):
     if self.session is None:
       await self.create_session()
 
-      async with self.session.get(url, **kwargs) as response:
-        if not (300 > response.status >= 200):
+    async with self.session.get(url, **kwargs) as response:
+      if not (300 > response.status >= 200):
           # TODO: Seperate exception for statuses raised
           raise ValueError("Raised " + str(response.status))
 
-          try:
-            content = await response.json()
-          except aiohttp.ClientResponseError:
-            content = await response.read()
+      try:
+        content = await response.json()
+      except aiohttp.ClientResponseError:
+        content = await response.read()
 
-          return content
+      return content
 
-  async def close(self):
-    if self.session is not None:
-      await self.session.close()
-      self.session = None
+async def close(self):
+  if self.session is not None:
+    await self.session.close()
+    self.session = None
