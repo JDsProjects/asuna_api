ba19c5eac52154edb7e2fdb112aa973ea236dc1a
JDJG Inc. Official <ghostzap123@gmail.com>
2021-01-02 23:19:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'asuna_api',
   packages = ['asuna_api'],
-  version = '0.1', 
+  version = '0.2', 
   license='MIT', 
   description = 'An async python wrapper for the asuna api',
   author = 'JDJGIncOfficial',
