bbc7a68bed57624fedb7e290a1a676c54c4182e6
JDJG Inc. Official <ghostzap123@gmail.com>
2021-01-03 02:30:20 -0500

    Update client.py

--- asuna_api/client.py
+++ asuna_api/client.py
@@ -2,8 +2,14 @@ import aiohttp
 from .http import HTTPClient
 from yarl import URL
 from .image import Image 
+import datetime
+from .minecraft import Minecraft
 
 class InputError(Exception):
+  __slots__ = ()
+  pass
+
+class InvalidUser(Exception):
   __slots__ = ()
   pass
 
@@ -25,6 +31,37 @@ class Client:
     response = await self._http_client.get(self.asuna_api_url(name))
     url = response.get("url")
     return Image(self._http_client, url)
+  
+  async def get_mchistory(self,username="JDJG_IncOfficial"):
+    if username is None:
+      raise InvalidUser(str(username) + " is not a valid option")
+    response = await self._http_client.get(self.mchistory_username(username))
+
+    if isinstance(response,dict):
+      namehistory = await self._http_client.get(self.mchistory_uuid(response.get("id")))
+      for x in namehistory:
+        if "changedToAt" in x.keys():
+          changed_at=datetime.datetime.utcfromtimestamp(int(x["changedToAt"])/1000).strftime('%Y-%m-%d')
+          x["changedToAt"] = changed_at
+        
+        if not "changedToAt" in x.keys():
+          x["changedToAt"] = "Original Name"
+           
+      mc_data = {"username":response["name"],"uuid":response["id"],"name_history":namehistory}
+     
+      return Minecraft(mc_data)
+
+    if isinstance(response,bytes):
+      raise InvalidUser(username + " is not a valid option")
+
+  def mchistory_username(self,username):
+    url = URL.build(scheme="https",host="api.mojang.com/users/profiles/minecraft",path="/"+username.lstrip("/"))
+    return str(url)
+  
+  def mchistory_uuid(self,uuid):
+    url = URL.build(scheme="https",host="api.mojang.com/user/profiles",path="/"+uuid+"/names")
+    
+    return str(url)
 
   async def close(self):
     await self._http_client.close()
