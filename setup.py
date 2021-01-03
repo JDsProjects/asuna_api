cad7c9346778b4b19d2cfaa3f4d74779a7a2f29a
JDJG Inc. Official <ghostzap123@gmail.com>
2021-01-02 21:43:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'JDJGIncOfficial',
   author_email = 'jdjgbot@gmail.com',
   url = 'https://github.com/JDJGInc/asuna_api',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  download_url = 'https://github.com/JDJGInc/asuna_api/archive/1.0.1.tar.gz',
   keywords = ['wrapper', 'api', 'random'],
   install_requires=['aiohttp','yarl'],
   classifiers=[
