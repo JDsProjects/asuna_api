3921b9cf4870ae45b6a7f254e88d2ab132efc8f5
JDJG Inc. Official <ghostzap123@gmail.com>
2021-01-07 23:11:03 -0500

    Update image.py

--- asuna_api/image.py
+++ asuna_api/image.py
@@ -4,10 +4,11 @@ from asuna_api.http import HTTPClient
 # heavily inspired by https://github.com/Rapptz/discord.py/blob/master/discord/asset.py
 #code from https://github.com/iDutchy/sr_api/blob/1c97c1b355f1ef12b1dc64a5e80ecba37ad3fc81/sr_api/image.py
 class Image:
-  __slots__ = ("url", "_http_client")
+  __slots__ = ("url", "_http_client","filename")
 
-  def __init__(self, http_client: HTTPClient, url):
+  def __init__(self, http_client: HTTPClient, url,filename):
     self.url = url
+    self.filename = filename
     self._http_client = http_client
 
   def __str__(self):
