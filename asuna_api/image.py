7a467a67366190cde42f4e404605e7648393804c
MrKomodoDragon <svrchn921@outlook.com>
2021-06-17 10:56:56 -0700

    smh jdjg

--- asuna_api/image.py
+++ asuna_api/image.py
@@ -2,31 +2,31 @@ import io
 from asuna_api.http import HTTPClient
 
 # heavily inspired by https://github.com/Rapptz/discord.py/blob/master/discord/asset.py
-#code from https://github.com/iDutchy/sr_api/blob/1c97c1b355f1ef12b1dc64a5e80ecba37ad3fc81/sr_api/image.py
+# code from https://github.com/iDutchy/sr_api/blob/1c97c1b355f1ef12b1dc64a5e80ecba37ad3fc81/sr_api/image.py
 class Image:
-  __slots__ = ("url", "_http_client","filename")
+    __slots__ = ("url", "_http_client", "filename")
 
-  def __init__(self, http_client: HTTPClient, url,filename):
-    self.url = url
-    self.filename = filename
-    self._http_client = http_client
+    def __init__(self, http_client: HTTPClient, url, filename):
+        self.url = url
+        self.filename = filename
+        self._http_client = http_client
 
-  def __str__(self):
-    return self.url if self.url is not None else ''
+    def __str__(self):
+        return self.url if self.url is not None else ""
 
-  async def read(self):
-    return await self._http_client.get(self.url)
+    async def read(self):
+        return await self._http_client.get(self.url)
 
-  async def save(self, fp, seek_start=True):
-    data = await self.read()
-    if isinstance(fp, io.IOBase) and fp.writable():
-      written = fp.write(data)
+    async def save(self, fp, seek_start=True):
+        data = await self.read()
+        if isinstance(fp, io.IOBase) and fp.writable():
+            written = fp.write(data)
 
-      if seek_start:
-        fp.seek(0)
+            if seek_start:
+                fp.seek(0)
 
-      return written
+            return written
 
-    else:
-      with open(fp, 'wb') as f:
-        return f.write(data)
+        else:
+            with open(fp, "wb") as f:
+                return f.write(data)
--- setup.py
+++ setup.py
@@ -1,32 +1,33 @@
 from setuptools import setup
+
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-  name = 'asuna_api',
-  packages = ['asuna_api'],
-  version = '0.2.6', 
-  license='MIT', 
-  description = 'An async python wrapper for the asuna api',
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  author = 'JDJGIncOfficial',
-  author_email = 'jdjgbot@gmail.com',
-  url = 'https://github.com/JDJGInc/asuna_api',
-  download_url = 'https://github.com/JDJGInc/asuna_api/archive/0.0.9.tar.gz',
-  keywords = ['wrapper', 'api', 'random'],
-  install_requires=['aiohttp','yarl'],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9'
-  ],
+    name="asuna_api",
+    packages=["asuna_api"],
+    version="0.2.6",
+    license="MIT",
+    description="An async python wrapper for the asuna api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="JDJGIncOfficial",
+    author_email="jdjgbot@gmail.com",
+    url="https://github.com/JDJGInc/asuna_api",
+    download_url="https://github.com/JDJGInc/asuna_api/archive/0.0.9.tar.gz",
+    keywords=["wrapper", "api", "random"],
+    install_requires=["aiohttp", "yarl"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
