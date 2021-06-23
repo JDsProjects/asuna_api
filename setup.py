8743be4a9b64447136a88f3cda17643bd7c1ee48
JDJG Inc. Official <ghostzap123@gmail.com>
2021-06-22 23:07:19 -0400

    There you go

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     download_url= "https://github.com/JDJGInc/asuna_api/archive/0.1.1.tar.gz",
     keywords= ["wrapper", "api", "random"],
     install_requires= ["aiohttp", "yarl"],
-    classifiers=[
+    classifiers= [
         "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Intended Audience :: Developers",  # Define that your audience are developers
         "Topic :: Software Development :: Build Tools",
